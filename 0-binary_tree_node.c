0707d12b8d228b526041511f8e89b348035003d2
VictorNalu <victorchinalurumogu@gmail.com>
2024-04-11 11:03:52 +0100

    tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,63 +10,19 @@
  *
  * Return: Pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	/* Allocate memory for the new node */
-	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-	{
-		/* Memory allocation failed */
-		return (NULL);
-	}
-	
-	/* Set the value and parent of the new node */
-	new_node->value = value;
-	new_node->parent = parent;
-	
-	/* Initialize the left and right children to NULL */
-	new_node->left = NULL;
-	new_node->right = NULL;
-	
-	/* Return a pointer to the new node */
-	return (new_node);
-}
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	/* Create nodes */
-	binary_tree_t *root = binary_tree_node(NULL, 98);
-	binary_tree_t *node_1 = binary_tree_node(root, 12);
-	binary_tree_t *node_2 = binary_tree_node(root, 402);
-	binary_tree_t *node_3 = binary_tree_node(node_1, 6);
-	binary_tree_t *node_4 = binary_tree_node(node_1, 16);
-	binary_tree_t *node_5 = binary_tree_node(node_2, 256);
-	binary_tree_t *node_6 = binary_tree_node(node_2, 512);
+/* Function to create a binary tree node */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL) {
+        return NULL; // Return NULL on failure to allocate memory
+    }
 
-	/* Connect nodes */
-	root->left = node_1;
-	root->right = node_2;
-	node_1->left = node_3;
-	node_1->right = node_4;
-	node_2->left = node_5;
-	node_2->right = node_6;
+    // Initialize the new node with the given value and parent
+    new_node->parent = parent;
+	new_node->n = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-	/* Print the tree structure */
-	binary_tree_print(root, 0);
-
-	/* Remember to free memory allocated for the nodes */
-	free(root);
-	free(node_1);
-	free(node_2);
-	free(node_3);
-	free(node_4);
-	free(node_5);
-	free(node_6);
-
-	return (0);
+    return (new_node); // Return a pointer to the new node
 }
--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,20 +14,17 @@
  */
 typedef struct binary_tree_s
 {
-    int value;
+    int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Function prototypes */
-binary_tree_t *binary_tree_node(
-    binary_tree_t *parent,
-    int value
-    );
-void binary_tree_print(
-    const binary_tree_t *tree,
-    int level
-    );
+/*Print Function Prototype*/
+void binary_tree_print(const binary_tree_t *);
+
+/* Other Function prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 
 #endif /* BINARY_TREES_H */
